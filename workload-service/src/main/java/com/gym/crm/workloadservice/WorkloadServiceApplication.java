0d195250bf51b29eac0690affc7510c47fda8f9a
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-09-04 22:03:37 +0300

    GCA-176 Implement Discovery server - register service to eureka server

--- workload-service/src/main/java/com/gym/crm/workloadservice/WorkloadServiceApplication.java
+++ workload-service/src/main/java/com/gym/crm/workloadservice/WorkloadServiceApplication.java
@@ -2,8 +2,10 @@ package com.gym.crm.workloadservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkloadServiceApplication {
 
     public static void main(String[] args) {
