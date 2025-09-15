4fc44d72323e235ec5ae6e0ecc231c9c94671584
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-09-15 16:53:18 +0300

    GCA-187 Migrate workload updates to ActiveMQ - Add ActiveMQ configuration

---
+++ workload-service/src/main/java/com/gym/crm/workloadservice/config/ActiveMQConfig.java
@@ -0,0 +1,39 @@
+package com.gym.crm.workloadservice.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.connection.CachingConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+@Configuration
+@EnableJms
+public class ActiveMQConfig {
+    @Value("${jms.request-timeout-ms:5000}")
+    private long jmsRequestTimeout;
+
+    @Bean
+    public JmsTemplate jmsTemplate(CachingConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setMessageConverter(messageConverter);
+        jmsTemplate.setDeliveryPersistent(true);
+        jmsTemplate.setReceiveTimeout(jmsRequestTimeout);
+
+        return jmsTemplate;
+    }
+
+    @Bean
+    public MessageConverter jacksonJmsMessageConverter(ObjectMapper objectMapper) {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setObjectMapper(objectMapper);
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+
+        return converter;
+    }
+}
