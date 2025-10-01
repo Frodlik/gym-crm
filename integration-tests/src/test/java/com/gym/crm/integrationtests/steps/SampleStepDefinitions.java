cda7a080a280e1aae3b34e6c8a21b985bd46901c
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-10-01 21:25:37 +0300

    GCA-214 Configure Cucumber test infrastructure - Refactor SampleStepDefinitions to improve variable naming for clarity

--- integration-tests/src/test/java/com/gym/crm/integrationtests/steps/SampleStepDefinitions.java
+++ integration-tests/src/test/java/com/gym/crm/integrationtests/steps/SampleStepDefinitions.java
@@ -7,17 +7,17 @@ import io.cucumber.java.en.When;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class SampleStepDefinitions {
-    private boolean applicationRunning;
+    private boolean isApplicationRunning;
     private String applicationStatus;
 
     @Given("the application is running")
     public void theApplicationIsRunning() {
-        applicationRunning = true;
+        isApplicationRunning = true;
     }
 
     @When("I check the application status")
     public void iCheckTheApplicationStatus() {
-        applicationStatus = applicationRunning ? "healthy" : "unhealthy";
+        applicationStatus = isApplicationRunning ? "healthy" : "unhealthy";
     }
 
     @Then("the application should be healthy")
