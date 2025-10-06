8ba55709df4c38c0c461c3ff07923007e8fd5de7
Vadym Zakletskyi <zakletskiy.vadim@kpi.ua>
2025-10-07 00:23:07 +0300

    GCA-217 Implement integration tests - Configure MySQL datasource and add database cleanup hooks

--- integration-tests/src/test/java/com/gym/crm/integrationtests/hook/DatabaseHooks.java
+++ integration-tests/src/test/java/com/gym/crm/integrationtests/hook/DatabaseHooks.java
@@ -1,14 +1,27 @@
 package com.gym.crm.integrationtests.hook;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @RequiredArgsConstructor
 public class DatabaseHooks {
     private final MongoTemplate mongoTemplate;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
-    @Before("@workload")
+    @After
+    public void cleanMySqlDatabase() {
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 0");
+        jdbcTemplate.queryForList("SHOW TABLES", String.class)
+                .forEach(table -> jdbcTemplate.execute("TRUNCATE TABLE " + table));
+        jdbcTemplate.execute("SET FOREIGN_KEY_CHECKS = 1");
+    }
+
+    @Before("@Workload")
     public void setUp() {
         mongoTemplate.getDb().drop();
     }
